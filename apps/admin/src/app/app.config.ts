2d6b204ecb0af2544d34d59297be3d781fc3dfd4
DennisMaj <charlesgift231@gmail.com>
2026-06-21 06:38:22 +0100

    fix: simplify app.config.ts - remove potentially problematic providers

--- apps/admin/src/app/app.config.ts
+++ apps/admin/src/app/app.config.ts
@@ -1,12 +1,20 @@
-import { ApplicationConfig } from '@angular/core';
+import {
+  ApplicationConfig,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { appRoutes } from './app.routes';
 import { authInterceptor } from './interceptors/auth.interceptor';
+import { appRoutes } from './app.routes';
+import { environment } from '../environments/environment';
+import { ENVIRONMENT_CONFIG } from '@serveiq/shared/data-access';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(appRoutes),
-    provideHttpClient(withInterceptors([authInterceptor]))
-  ]
+    provideHttpClient(withInterceptors([authInterceptor])),
+    {
+      provide: ENVIRONMENT_CONFIG,
+      useValue: environment,
+    },
+  ],
 };
