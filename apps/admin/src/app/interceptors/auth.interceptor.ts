0c34ab3ca180d9238a9bf71e23cd31c5248c8dc4
DennisMaj <charlesgift231@gmail.com>
2026-06-21 11:34:19 +0100

    debug: ultra verbose interceptor logs

--- apps/admin/src/app/interceptors/auth.interceptor.ts
+++ apps/admin/src/app/interceptors/auth.interceptor.ts
@@ -1,18 +1,20 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  console.log('!!! INTERCEPTOR START !!! URL:', req.url);
   const token = localStorage.getItem('accessToken') || localStorage.getItem('token');
-  console.log('[Auth Interceptor] Intercepting request:', req.url);
-  console.log('[Auth Interceptor] Token found:', !!token);
+  console.log('!!! INTERCEPTOR TOKEN !!!', !!token);
   
   if (token) {
     const authReq = req.clone({
-      headers: req.headers.set('Authorization', `Bearer ${token}`)
+      setHeaders: {
+        Authorization: `Bearer ${token}`
+      }
     });
-    console.log('[Auth Interceptor] Authorization header attached');
+    console.log('!!! INTERCEPTOR ATTACHED !!!');
     return next(authReq);
   }
   
-  console.warn('[Auth Interceptor] No token found in localStorage');
+  console.warn('!!! INTERCEPTOR NO TOKEN !!!');
   return next(req);
 };
